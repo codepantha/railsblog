7e3a4430488d4de2de2ae71a03c0681de73e848f
codepanther <pezechigozirim@gmail.com>
2022-02-25 20:00:12 +0100

    Create comment via a form funtionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.find(params[:post_id])
+
+    @comment = Comment.new(user: @user, post: @post, text: params[:comment][:text])
+    if @comment.save
+      redirect_to user_post_url({ user_id: @user.id, id: @post.id })
+      flash[:success] = 'Post created successfully'
+    else
+      render :new, flash: { error: 'Something went wrong with your post' }
+    end
+  end
+end
