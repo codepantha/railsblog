6df77d1a424d825dc0d52f9b6cb67820e8b11a95
codepanther <pezechigozirim@gmail.com>
2022-02-25 19:16:24 +0100

    Create post with via a form functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,19 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @post = Post.new(user: @user, title: params[:post][:title], text: params[:post][:text])
+    if @post.save
+      redirect_to user_post_url({ user_id: @user.id, id: @post.id })
+      flash[:success] = 'Post created successfully'
+    else
+      render :new, flash: { error: 'Something went wrong with your post' }
+    end
+  end
 end
