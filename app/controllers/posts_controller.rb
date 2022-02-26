722008acf3d66d26958c886c185e834ef3a413b2
codepanther <pezechigozirim@gmail.com>
2022-02-26 10:25:48 +0100

    Account for the model validation in the posts_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,12 +17,16 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = Post.new(post_params)
     @post.user = @user
+    @post.comments_counter = 0
+    @post.likes_counter = 0
 
-    if @post.save
+    if @post.valid?
+      @post.save
       redirect_to user_post_url({ user_id: @user.id, id: @post.id })
       flash[:success] = 'Post created successfully'
     else
-      render :new, flash: { error: 'Something went wrong with your post' }
+      render :new
+      flash.now[:notice] = 'Something went wrong with your post'
     end
   end
 
