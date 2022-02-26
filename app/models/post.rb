454c88381eae6fad4e22bbde0faa822be59010b1
codepanther <pezechigozirim@gmail.com>
2022-02-26 08:10:02 +0100

    Add model validations for Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,15 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :title, presence: { message: 'Title is required' }
+  validates :title, length: { maximum: 250, message: 'Title must not exceed 250 characters' }
+  validates :comments_counter,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                            message: 'comments_counter must be greater than or equal to 0' }
+  validates :likes_counter,
+            numericality: { only_integer: true, greater_than_or_equal_to: 0,
+                            message: 'likes_counter must be greater than or equal to 0' }
+
   # update a user's posts_counter
   def self.update_user_posts_counter(user)
     user.update(posts_counter: user.posts.count)
